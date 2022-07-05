e05b3b87850e7608d6243b2cfa9367c6d4ee2ece
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-07-05 20:35:17 +0530

    Add:images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import "./App.css";
 
 import { Navbar } from "./components";
+import { MainPage } from "./pages";
 
 function App() {
   return (
     <>
       <Navbar />
+      <MainPage />
       {/* <div className='container mx-auto bg-gray-200 rounded-xl'>
         <h1 className='text-3xl font-bold mb-5'>Hello world!</h1>
       </div> */}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import hero from "../Assets/hero.png";
+import prog1 from "../Assets/prog1.png";
+import prog2 from "../Assets/prog2.png";
+import prog3 from "../Assets/prog3.png";
+import prog4 from "../Assets/prog4.png";
+
+const MainPage = () => {
+  return (
+    <div className='flex flex-col  items-center min-h-screen p-7'>
+      <div className='flex flex-col'>
+        <div className='text-center font-extrabold text-4xl'>
+          Welcome to frontend Quiz
+        </div>
+        <img src={hero} alt='hero-img' />
+      </div>
+      <div className='flex-col'>
+        <div className='flex gap-2.5'>
+          <div className='relative hover:shadow-lg shadow-gray-400/50'>
+            <img className='max-w-xs h-80 ' src={prog1} alt='html-img' />
+            <div className='text-center font-semibold text-md'>HTML</div>
+          </div>
+          <div className='flex flex-col  hover:shadow-lg shadow-gray-400/50'>
+            <img className='max-w-xs h-80' src={prog2} alt='css-img' />
+            <div className='text-center align-text-bottom items-end font-semibold text-md'>
+              CSS
+            </div>
+          </div>
+        </div>
+        <div className='flex gap-2.5'>
+          <div className='flex flex-col  hover:shadow-lg shadow-gray-400/50'>
+            <img className='max-w-xs h-80' src={prog3} alt='js-img' />
+            <div className='text-center align-text-bottom items-end font-semibold text-md'>
+              Javascript
+            </div>
+          </div>
+          <div className='flex flex-col  hover:shadow-lg shadow-gray-400/50'>
+            <img className='max-w-xs h-80' src={prog4} alt='react-img' />
+            <div className='text-center align-text-bottom items-end font-semibold text-md'>
+              React
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { MainPage };
