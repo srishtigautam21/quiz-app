4f7322fe91318003f768097040f73189b5226a03
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-09-19 22:31:41 +0530

    New files

---
+++ src/data/JsData.js
@@ -0,0 +1,14 @@
+const JsData = [
+  {
+    id: 1,
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: {
+      option1: "<javascript>",
+      option2: "<js>",
+      option3: "<script>",
+      option4: "<scripting>",
+    },
+    answer: "option3",
+  },
+];
+export { JsData };
