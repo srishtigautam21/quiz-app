5843d70efef953a8ddff46866c78b12863a4717c
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2023-09-19 22:30:49 +0530

    Add jsquestions

--- src/pages/JsPage.jsx
+++ src/pages/JsPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { JsData } from "../data/JsData";
+import { useState } from "react";
+import "./JsPage.css";
 
 const JsPage = () => {
-  return <div>JsPage</div>;
+  const [jsData, setJsData] = useState(JsData);
+  return (
+    <div className='wrapper'>
+      {jsData.map((ques, index) => {
+        return (
+          <div className='quesBackground'>
+            <div>{ques.question}</div>
+            <div>
+              <div>{ques.options.option1}</div>
+              <div>{ques.options.option2}</div>
+              <div>{ques.options.option3}</div>
+              <div>{ques.options.option4}</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export { JsPage };
